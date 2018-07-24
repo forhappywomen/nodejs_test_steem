e77264693d55e006cd238c4691806a88a5d0a1d7
forhappywomen <forhappywomen@gmail.com>
2018-07-24 15:15:17 +0900

    추가

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("hello world!!");
+var steem = require("steem");
+
+steem.api.setOptions({url: 'https://api.steemit.com'});
+steem.api.getAccounts(['forhappywomen'], function(err, response){
+    console.log(err, response);
+
+});
